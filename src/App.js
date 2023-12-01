d43cf575a1f0c0b17a6492107afa91b026652269
makihaya <93355988+makihaya@users.noreply.github.com>
2023-11-30 20:09:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,25 @@ https://uselessfacts.jsph.pl/
 
 function App() {
   const [fact, setfact] = useState(null);
+  const [typeInput, setTypeInput] = useState('');
+  const [substring1, setSubstring1] = useState('');
+  const [substring2, setSubstring2] = useState('');
 
   function handleClick() {
     const request = new XMLHttpRequest();
     request.open('GET', 'https://uselessfacts.jsph.pl/api/v2/facts/random');
     request.onload = function() {
       if (request.status === 200) {
-        setfact(JSON.parse(request.responseText).text)
+        setfact(JSON.parse(request.responseText).text);
       }
     };
     request.send();
   }
 
   const handleChange = (event) =>{
-    console.log(event.target.value)
+    setTypeInput(event.target.value)
+    setSubstring1(fact.slice(0, event.target.value.length))
+    setSubstring2(fact.slice(event.target.value.length, fact.length))
   }
 
   return (
